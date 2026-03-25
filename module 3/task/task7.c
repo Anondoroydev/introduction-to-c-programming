13b16ce7c4c5d58a2f195c5d3d64f648c946272a
Ajoy Sarkar <155249242+Anondoroydev@users.noreply.github.com>
2026-03-25 13:36:51 +0600

    module done

---
+++ module 3/task/task7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a % b == 0)
+    {
+        printf("Yes");
+    }
+    else if (b % a == 0)
+    {
+        printf("Yes");
+    }
+
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
