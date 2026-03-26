4f527a368aed58336576d1674550490265f9d481
Ajoy Sarkar <155249242+Anondoroydev@users.noreply.github.com>
2026-03-27 01:48:40 +0600

    slove problem

---
+++ module 6/practice.c
@@ -0,0 +1,34 @@
+ 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x = 10; // x এর মান শুরুতে 10
+
+//     int y = x++; // post-increment:
+//                  // আগে x এর মান (10) y তে যাবে
+//                  // তারপর x = 11 হবে
+
+//     int z = ++y; // pre-increment:
+//                  // আগে y = 10 থেকে 11 হবে
+//                  // তারপর সেই 11 z তে যাবে
+
+//     printf("%d", z++); // আগে z এর মান (11) print হবে
+//                        // তারপর z = 12 হবে (কিন্তু সেটা print হয় না)
+
+//     return 0;
+// }
+
+//! pre_incement
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x = 10;
+//     ++x;
+//     printf("%d", ++x);
+//     return 0;
+// }
+
+
