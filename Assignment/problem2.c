92961c035dda1a990a7466ef45365a8d75d5811f
Ajoy Sarkar <155249242+Anondoroydev@users.noreply.github.com>
2026-03-26 15:06:14 +0600

    assignment done

---
+++ Assignment/problem2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    long long a;
+    long long b;
+
+    scanf("%lld %lld", &a, &b);
+    long long sum = a * b;
+
+    printf("%lld", sum);
+
+    return 0;
+}
