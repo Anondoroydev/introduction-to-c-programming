026a52361f6735a7f41abecb1b2f5fa16a4d7ba3
Anondo Ray <ajoysarker553@gmail.com>
2026-04-14 01:01:03 +0600

    More issues resolved.

---
+++ module 6/D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    if ((a + b == d) || (a - b == d) || (a * b == d) || (b != 0 && a / b == d) ||
+        (b + c == d) || (b - c == d) || (b * c == d) || (c != 0 && b / c == d) ||
+        (a + c == d) || (a - c == d) || (a * c == d) || (c != 0 && a / c == d))
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+    return 0;
+}
